b1af6071d6d7716e5552c579e77ef3d85f6e8e77
Priscilla-Odame <42655518+Priscilla-Odame@users.noreply.github.com>
2020-07-04 22:12:20 +0100

    Add files via upload

---
+++ jedyy/pytorch/try.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 24 22:40:36 2020
+
+@author: BOLD I.T
+"""
+
+#import torchvision.models as models
+import tensorflow as tf
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import datetime
+
+#mnist = tf.keras.datasets.mnist
+#mobilenet = models.mobilenet_v2(pretrained=True,progress=True)
+
+class HamDataset(Dataset):
+
+    def __init__(self):
+        # Initialize data, download, etc.
+        # read with numpy or pandas
+        xy = np.loadtxt('C:/Users/BOLD I.T/Desktop/jedyy/pytorch/HAMDataset/hmnist_28_28_RGB.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        self.n_samples = xy.shape[0]
+
+        # here the first column is the class label, the rest are the features
+        self.x_data = torch.from_numpy(xy[:, 1:]) # size [n_samples, n_features]
+        self.y_data = torch.from_numpy(xy[:, [0]]) # size [n_samples, 1]
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
+dataset=HamDataset()
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=4,
+                          shuffle=True,
+                          num_workers=0)
+
+(x_train, y_train),(x_test, y_test) = DataLoader()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+def create_model():
+  return tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
+    
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+train_dataset = train_dataset.shuffle(60000).batch(64)
+test_dataset = test_dataset.batch(64)
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+# Define our metrics
+train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('train_accuracy')
+test_loss = tf.keras.metrics.Mean('test_loss', dtype=tf.float32)
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('test_accuracy')
+
+def train_step(model, optimizer, x_train, y_train):
+  with tf.GradientTape() as tape:
+    predictions = model(x_train, training=True)
+    loss = loss_object(y_train, predictions)
+  grads = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(y_train, predictions)
+
+def test_step(model, x_test, y_test):
+  predictions = model(x_test)
+  loss = loss_object(y_test, predictions)
+
+  test_loss(loss)
+  test_accuracy(y_test, predictions)
+  
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
+test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+test_summary_writer = tf.summary.create_file_writer(test_log_dir)
+
+model = create_model() # reset our model
+
+EPOCHS = 5
+
+for epoch in range(EPOCHS):
+  for (x_train, y_train) in train_dataset:
+    train_step(model, optimizer, x_train, y_train)
+  with train_summary_writer.as_default():
+    tf.summary.scalar('loss', train_loss.result(), step=epoch)
+    tf.summary.scalar('accuracy', train_accuracy.result(), step=epoch)
+
+  for (x_test, y_test) in test_dataset:
+    test_step(model, x_test, y_test)
+  with test_summary_writer.as_default():
+    tf.summary.scalar('loss', test_loss.result(), step=epoch)
+    tf.summary.scalar('accuracy', test_accuracy.result(), step=epoch)
+  
+  template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+  print (template.format(epoch+1,
+                         train_loss.result(), 
+                         train_accuracy.result()*100,
+                         test_loss.result(), 
+                         test_accuracy.result()*100))
+
+  # Reset metrics every epoch
+  train_loss.reset_states()
+  test_loss.reset_states()
+  train_accuracy.reset_states()
+  test_accuracy.reset_states()
